40d1c863bb67c2528fb5a8a287dd362673b29e9c
r60019zj <zain.jaffal@student.manchester.ac.uk>
2019-11-05 17:25:43 +0000

    final version for the spellchecker5 without comments

--- spellchecker5.py
+++ spellchecker5.py
@@ -1,52 +1,64 @@
-import datetime
-import time
+import datetime, time
 import os
 from difflib import SequenceMatcher
-sizeX=50
-def stringMargin(listofItems):
-    while len(listofItems) < 8 :
-        listofItems.append("")
-    size = sizeX-1
-    modifiedList = []
-    for item in listofItems:
-        needed_length =size - len(item)
-        rightIntedent = 10
-        if item == listofItems[0] :
-            rightIntedent = 5
 
-        leftIntedent = needed_length - rightIntedent
-        leftMargin = len(item) + leftIntedent
-        item = item.ljust(leftMargin)
-        rightMargin= len(item) + rightIntedent
-        item = item.rjust(rightMargin," ")
+sizeX = 50  # predefined width for the box that will contain items
+
+
+# function stringMargin
+# takes an input of list of contents that need to be printed in a box
+# creates an artificial spaces around the item
+# if the list is less than 8 it will add empty string items
+# to keep the box at a standard height
+
+def stringMargin(listOfItemsForBox):
+    # adds empty elements to the list
+    while len(listOfItemsForBox) < 8:
+        listOfItemsForBox.append("")
+    size = sizeX - 1  # the general size the item should have
+    modifiedList = []  # the final output of the function
+    for item in listOfItemsForBox:
+        needed_length = size - len(item)  # calculates needed length
+        rightIndent = 10   # a standard right intended
+        if item == listOfItemsForBox[0]:
+            rightIndent = 5   # if the item was the first item the margin is 5
+
+        leftIndent = needed_length - rightIndent
+        leftMargin = len(item) + leftIndent  # adds the left indent to total length of item
+        item = item.ljust(leftMargin)  # uses the leftmargin to add spaces to the left side of the item
+        rightMargin = len(item) + rightIndent  # adds the right indent to total len of item
+        item = item.rjust(rightMargin, " ")  # adds right indent spaces into the item
         modifiedList.append(item)
     return modifiedList
 
-def boxGenerator(listofContent) :
-    listofContent = stringMargin(listofContent)
-    print(u'\u250F', end="", flush=True) #corner
-    for i in range (0,sizeX-1) :
-        print(u'\u2501' , end="", flush=True) #top-bar
-    print(u'\u2513') #corner
 
-    for i in range (0,len(listofContent)):
-    #down
-        print(u'\u2503', end="", flush=True) #vertical pipe
+def boxGenerator(listOfContent):
+    os.system('clear')
+    listOfContent = stringMargin(listOfContent)
+    print(u'\u250F', end="", flush=True)  # corner
+    for i in range(0, sizeX - 1):
+        print(u'\u2501', end="", flush=True)  # top-bar
+    print(u'\u2513')  # corner
 
-        print(listofContent[i] ,end="", flush=True)
+    for i in range(0, len(listOfContent)):
+        # down
+        print(u'\u2503', end="", flush=True)  # vertical pipe
 
-        print(u'\u2503') #vertical pipe
+        print(listOfContent[i], end="", flush=True)
 
-    print(u'\u2523', end="", flush=True) #T style pipe
+        print(u'\u2503')  # vertical pipe
 
-    for i in range (0,sizeX-1) :#bottom
-        print(u'\u2501' , end="", flush=True) #horizontal pipe
+    print(u'\u2523', end="", flush=True)  # T style pipe
 
-    print(u'\u251B') # corner
+    for i in range(0, sizeX - 1):  # bottom
+        print(u'\u2501', end="", flush=True)  # horizontal pipe
 
-    print(u'\u2517',end="", flush=True)
-    for i in range(0,20):
-        print(u'\u2501' , end="", flush=True)
+    print(u'\u251B')  # corner
+
+    print(u'\u2517', end="", flush=True)
+    for i in range(0, 20):
+        print(u'\u2501', end="", flush=True)
+
 
 terminated = False
 englishDictReadOnly = open("EnglishWords.txt")
@@ -55,137 +67,144 @@ englishDictReadOnly.close()
 englishDict = open("EnglishWords.txt", "a")  # opens the file of the english words
 
 
-# checks if the sentence contains any non alpha characters and removes them
-# returns a sentence without non-alpha characters
-# could be done using isAlpha function
 def sentenceModification(sentence):
     sentence = sentence.lstrip().rstrip().lower()
     for chars in sentence:
-        if (ord(chars) < 97 or ord(chars) > 122) and ord(chars) != 32:
-            sentence = sentence.replace(chars, "")
-
+        if (ord(chars) < ord("a") or ord(chars) > ord("z")) and ord(chars) != ord(" "):
+            sentence = sentence.replace(chars, " ")
     return sentence
 
 
-def checkWords(sentence):
-    sentence=sentenceModification(sentence)
+fileWordsInformation = {
+    'total Number of words': 0,
+    'correctly spelt words': 0,
+    'incorrectly spelt words': 0,
+    'ignored words': 0,
+    'marked words': 0,
+    'words added to dictionary': 0
+}
+
+
+def checkWordsForSentences(sentence):
+    sentence = sentenceModification(sentence)
     listofWords = sentence.split()
-    correctWordsCount = 0
-    incorrectWordsCount = 0
+    fileWordsInformation["total Number of words"] = len(listofWords)
+    fileWordsInformation["correctly spelt words"] = 0
+    fileWordsInformation["incorrectly spelt words"] = 0
     for word in listofWords:
         if word in EnglishWords:
-            boxGenerator(["W O R D     F O U N D","","",word,"Spelt correctly"])
-            print("\n")
-            correctWordsCount += 1
+            boxGenerator(["W O R D     F O U N D", "", "", word, "Spelt correctly"])
+            dummyInput = input("enter any key to go next")
+            fileWordsInformation["correctly spelt words"] += 1
         else:
-            boxGenerator(["W O R D   N O T  F O U N D","","",word,"Not found in dictionary"])
-            incorrectWordsCount += 1
-    boxGenerator(["S U M M A R R Y","","","Number of words: " + str(len(listofWords)),"Number of correctly spelt words"])
-    print("Number of words: %d" % len(listofWords))
-    print("Number of correctly spelt words: %d" % correctWordsCount)
-    print("Number of incorrectly spelt words: %d" % incorrectWordsCount)
+            boxGenerator(["W O R D   N O T  F O U N D", "", "", word, "Not found in dictionary"])
+            dummyInput = input("enter any key to go next")
+            fileWordsInformation["incorrectly spelt words"] += 1
+    finalBoxList = ["F I N A L   R E S U L T", ""]
+    for x, y in fileWordsInformation.items():
+        tempStr = "Number of " + x + " : " + str(y)
+        finalBoxList.append(tempStr)
+        if len(finalBoxList) == 4:
+            break
+    boxGenerator(finalBoxList)
 
 
 def checkWordsForFiles(sentence, file):
-    sentence=sentenceModification(sentence)
+    sentence = sentenceModification(sentence)
     listofWords = sentence.split()
+    fileWordsInformation["total Number of words"] = len(listofWords)
     correctedWord = ""
-    checkedFile = open("2" + file, "w")
-    correctWordsCount = 0
-    incorrectWordsCount = 0
-    ignoredWordsCount = 0
-    markedWordsCount = 0
-    addedToDictionaryCount = 0
-
+    resultFile = open("2" + file, "w")
     for word in listofWords:
         if word in EnglishWords:
             correctedWord += word + " "
-            correctWordsCount += 1
+            fileWordsInformation["correctly spelt words"] += 1
         else:
-            print("\n%s not found in dictionary \n" % word)
-            print("1. Ignore the word \n2. Mark the word as incorrect." +
-                  "\n3. Add word to dictionary.\n")
-            incorrectWordsCount += 1
-            incorrectWordsHandling = input("Enter choice: ")
-            correctInput = False
-            while not correctInput:
-                if incorrectWordsHandling == "1":
-                    correctedWord += "!" + word + "! "
-                    ignoredWordsCount += 1
-                    correctInput = True
-                elif incorrectWordsHandling == "2":
-                    correctedWord += "?" + word + "? "
-                    markedWordsCount += 1
-                    correctInput = True
-                elif incorrectWordsHandling == "3":
-                    correctedWord += "*" + word + "* "
-                    addedToDictionaryCount += 1
-                    englishDict.write("\n" + word)
-                    correctInput = True
-
+            listOfRaitos = []
+            wordsFromRatios = []
+            for matchedWord in EnglishWords:
+                ratio = SequenceMatcher(None, matchedWord, word).ratio()
+                listOfRaitos.append(ratio)
+                wordsFromRatios.append(matchedWord)
+            desiredInput = False
+            while not desiredInput:
+                requiredWordIndex = listOfRaitos.index(max(listOfRaitos))
+                desiredWord = wordsFromRatios[requiredWordIndex]
+                boxGenerator(["W O R D   N O T   F O U N D", "", word, "", "did you mean", "", desiredWord])
+                changeWordInput = input("y or n else will go to the next screen :")
+                print("\n")
+                if changeWordInput == "y":
+                    correctedWord += desiredWord + " "
+                    desiredInput = True
+                elif changeWordInput == "n":
+                    listOfRaitos.pop(requiredWordIndex)
+                    wordsFromRatios.pop(requiredWordIndex)
                 else:
-                    print("please try again")
-                    print("1. Ignore the word \n2. Mark the word as incorrect." +
-                          "\n3.Add word to dictionary.\n")
-                    incorrectWordsHandling = input("Enter choice")
+                    desiredInput = True
+                    correctInput = False
+                    while not correctInput:
+                        boxGenerator(["W O R D   N O T   F O U N D", "", word, "", "1. Ignore the word.",
+                                      "2. Mark the word as incorrect.", "3. Add word to dictionary."])
+                        fileWordsInformation["incorrectly spelt words"] += 1
+                        incorrectWordsHandling = input("Enter choice: ")
+                        print("\n")
+                        if incorrectWordsHandling == "1":
+                            correctedWord += "!" + word + "! "
+                            fileWordsInformation["ignored words"] += 1
+                            correctInput = True
+                        elif incorrectWordsHandling == "2":
+                            correctedWord += "?" + word + "? "
+                            fileWordsInformation["marked words"] += 1
+                            correctInput = True
+                        elif incorrectWordsHandling == "3":
+                            correctedWord += "*" + word + "* "
+                            fileWordsInformation["words added to dictionary"] += 1
+                            englishDict.write("\n" + word)
+                            correctInput = True
 
-    checkedFile.write(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S\n"))
-
-    print("Number of words: %d" % len(listofWords))
-    checkedFile.write("Number of words: %d \n" % len(listofWords))
-    print("Number of correctly spelt words: %d" % correctWordsCount)
-    checkedFile.write("Number of correctly spelt words: %d \n" % correctWordsCount)
-    print("Number of incorrectly spelt words: %d" % incorrectWordsCount)
-    checkedFile.write("Number of incorrectly spelt words: %d \n" % incorrectWordsCount)
-    print("    Number of ignored words: %d" % ignoredWordsCount)
-    checkedFile.write("    Number of ignored: %d \n" % ignoredWordsCount)
-    print("    Number of added to dictionary: %d" % addedToDictionaryCount)
-    checkedFile.write("    Number of added to dictionary: %d \n" % addedToDictionaryCount)
-    print("    Number of marked: %d" % markedWordsCount)
-    checkedFile.write("    Number of marked: %d \n\n" % markedWordsCount)
-    checkedFile.write(correctedWord)
-    checkedFile.close()
+    resultFile.write(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S\n"))
+    finalBoxList = ["F I N A L   R E S U L T", ""]
+    for x, y in fileWordsInformation.items():
+        tempStr = "Number of " + x + " : " + str(y)
+        temp2Str = tempStr + "\n"
+        finalBoxList.append(tempStr)
+        resultFile.write(temp2Str)
+    boxGenerator(finalBoxList)
+    dummyInput = input("enter any key to go next")
+    resultFile.write(correctedWord)
+    resultFile.close()
 
 
 while not terminated:
     listOfWords = []  # creates a list to contain the words
-
-    boxGenerator(["S P E L L   C H E C K E R","","1. Check a file","2. Check a sentence","","","0.Quit"])
+    boxGenerator(["S P E L L   C H E C K E R", "", "1. Check a file", "2. Check a sentence", "", "", "0.Quit"])
     usrInput = input("Enter choice: ")
     if usrInput == "1":
         fileWrittenCorrect = False
-        boxGenerator(["L O A D    F I L E","","Enter a file name","then press [enter]"])
-        userFileNameInput = input(" Filename: ")
-        startTime = time.clock()
+        userFileNameInput = ""
         while not fileWrittenCorrect:
+            boxGenerator(["L O A D    F I L E", "", "Enter a file name", "then press [enter]"])
+            if userFileNameInput == "":
+                userFileNameInput = input(" Filename: ")
+            startTime = time.clock()
             if os.path.exists(userFileNameInput):
-                boxGenerator(["L O A D    F I L E","","","Correct File name"])
-
+                boxGenerator(["L O A D    F I L E", "", "", "Correct File name", "", "Done"])
                 checkWordsForFiles(open(userFileNameInput).read(), userFileNameInput)
                 fileWrittenCorrect = True
-                print("Done")
                 open(userFileNameInput).close()
                 endTime = time.clock() - startTime
-                print("TimeElapsed ", endTime * 10 ** 6, "microseconds")
-                usrInput = input("Press q [enter] to quit or any other key [enter] to go again: ")
-                if usrInput == "q":
-                    terminated = True
+                boxGenerator(["C H E C K E D   F I L E", "", "TimeElapsed", str(endTime * 10 ** 6), "microseconds"])
             else:
-                print("\nplease try again enter a file name ")
-                userExitFileMode = input("\nExit press q , rewrite the file name press any thing else ")
-                if userExitFileMode == "q":
+                boxGenerator(["L O A D    F I L E", "", "Enter a file name or press q to quit", "then press [enter]"])
+                userFileNameInput = input(" Filename: ")
+                if (userFileNameInput == "q"):
                     break
-                else:
-                    userFileNameInput = input("\nEnter the name of the file to spellcheck: ")
     elif usrInput == "2":
-
-        checkWords(input("\nEnter sentence to spellcheck: "))
-        usrInput = input("\nPress q [enter] to quit or any other key [enter] to go again: ")
+        boxGenerator(["S E N T E N C E   M O D E", "", "Enter a sentence", "then press [enter]"])
+        checkWordsForSentences(input("Enter sentence to spellcheck: "))
+        usrInput = input("Press q [enter] to quit or any other key [enter] to go again: ")
         if usrInput == "q":
             terminated = True
     elif usrInput == "0" or usrInput == "q":
         terminated = True
         englishDict.close()
-
-    else:
-        print("\nplease try again\n")
