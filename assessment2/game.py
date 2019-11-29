ae5334372e706a63a130f2a59ef77c2f8019ba2b
Zain Alden Jaffal <r60019zj@student.manchester.ac.uk>
2019-11-29 12:36:36 +0000

    added a line that follows a mouse

--- assessment2/game.py
+++ assessment2/game.py
@@ -5,12 +5,15 @@ y = 0
 def mouse_movement (event):
     x = canvas.canvasx(event.x)
     y = canvas.canvasy(event.y)
-    canvas.coords(1,x,y,x-50,y-70)
+    canvas.coords(2,110,230,x,y)
     canvas.update()
 
 main_window = Tk()
 canvas = Canvas(main_window,width=300,height=300)
 rectangle = canvas.create_rectangle(0, 0, 50, 70, fill="black")
+canvas.move(rectangle,60,230)
+aim_line = canvas.create_line(0,50,50,0)
+canvas.coords(aim_line,110,230,160,200)
 canvas.bind('<Motion>',mouse_movement)
 canvas.pack()
 
