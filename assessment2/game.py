68b0cbc902776c3b23e4005aae47c6657fc4fc61
Zain Alden Jaffal <r60019zj@student.manchester.ac.uk>
2019-12-01 16:56:25 +0000

    added methods for the menu items  + a pause button

--- assessment2/game.py
+++ assessment2/game.py
@@ -2,12 +2,18 @@ from tkinter import Tk, Canvas, Menu
 
 def mouse_movement (event):
     return canvas.coords(event)
-
+def res ():
+    pass
+def change_binding():
+    pass
+def pause_game():
+    pass
 main_window = Tk()
 main_menu = Menu(main_window)
 main_window.configure(menu = main_menu)
 
 sub_menu = Menu(main_menu)
+main_menu.add_command(label = 'pause', command = pause_game)
 main_menu.add_cascade(label = 'settings', menu = sub_menu)
 sub_menu.add_command(label = 'resolution', command = res)
 sub_menu.add_command(label = 'change keys', command = change_binding)
